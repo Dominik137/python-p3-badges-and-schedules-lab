190f38079b4cef950a9578aa2c8d549a1768a47f
Dominik Arment <domarment@gmail.com>
2024-04-02 10:33:22 -0400

    dome

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,28 @@
 def badge_maker(name):
-    return None
+   return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    badge_messages = []
+    for name in names:
+        badge_messages.append(f'Hello, my name is {name}.')
+    return badge_messages
+
 
 def assign_rooms(names):
-    return None
+    room_assignments = []
+    for i, name in enumerate(names, start=1):
+        room_assignments.append(f'Hello, {name}! You\'ll be assigned to room {i}!')
+    return room_assignments
+
+
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    for badge in badges:
+        print(badge)
+    room_assignments = assign_rooms(names)
+    for assignment in room_assignments:
+        print(assignment)
+
+
+printer(["ariel", "Greg"])
